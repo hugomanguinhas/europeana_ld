d778ecb40fb3ba71fcd310a7e0f223ab7c245926
Hugo Manguinhas <hugo.manguinhas@europeana.eu>
2016-10-18 09:09:09 +0200

    LD Toolkit

---
+++ ld-toolkit/src/main/java/eu/europeana/ld/toolkit/DumpCmd.java
@@ -0,0 +1,154 @@
+/**
+ * 
+ */
+package eu.europeana.ld.toolkit;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.OptionBuilder;
+import org.apache.commons.cli.OptionGroup;
+import org.apache.commons.cli.Options;
+import org.apache.commons.io.FileUtils;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.Resource;
+
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoDatabase;
+
+import eu.europeana.ld.edm.io.EDMTurtleWriter;
+import eu.europeana.ld.harvester.HarvesterCallback;
+import eu.europeana.ld.mongo.MongoEDMHarvester;
+
+/**
+ * @author Hugo Manguinhas <hugo.manguinhas@europeana.eu>
+ * @since 17 Oct 2016
+ */
+public class DumpCmd extends ToolkitCmd
+{
+    static { loadProperties("/etc/dump/config.xml"); }
+
+    public static void main(String[] args) { new DumpCmd().process(args); }
+
+    @SuppressWarnings("static-access")
+    protected Options buildOptions()
+    {
+        Options options = new Options();
+        options.addOption(new Option("help", getProperty("info.option.help")));
+        options.addOption(OptionBuilder.withArgName("host")
+                .hasArg()
+                .withDescription(getProperty("info.option.host"))
+                .isRequired()
+                .create("host"));
+        options.addOption(OptionBuilder.withArgName("port")
+                .hasArg()
+                .withDescription(getProperty("info.option.port"))
+                .withType(Integer.class)
+                .create("port"));
+
+        OptionGroup group = new OptionGroup();
+        group.setRequired(true);
+        group.addOption(OptionBuilder
+                .withDescription(getProperty("info.option.all"))
+                .create("all"))
+             .addOption(OptionBuilder
+                .withArgName("uris")
+                .hasArgs()
+                .withValueSeparator(',')
+                .withDescription(getProperty("info.option.uris"))
+                .create("uris"))
+             .addOption(OptionBuilder
+                .withArgName("file")
+                .hasArg()
+                .withDescription(getProperty("info.option.file"))
+                .create("file"))
+             .addOption(OptionBuilder
+                .withArgName("query")
+                .hasArg()
+                .withDescription(getProperty("info.option.search"))
+                .create("search"));
+        options.addOptionGroup(group);
+
+        options.addOption(OptionBuilder.withArgName("out")
+                .hasArg()
+                .withDescription(getProperty("info.option.file"))
+                .isRequired()
+                .create("out"));
+        return options;
+    }
+
+    @Override
+    protected void process(CommandLine line) throws Throwable
+    {
+        MongoEDMHarvester harv   = getHarvester(line);
+        File              out    = getOutput(line);
+        EDMTurtleWriter   writer = new EDMTurtleWriter();
+        HarvesterCallback cb     = getCallback(line, writer);
+
+        writer.start(out);
+        try                 { handleCmd(harv, cb, line);       }
+        finally             { /*client.close();*/ writer.finish(); }
+    }
+
+    protected File getOutput(CommandLine line)
+    {
+        File file = new File(line.getOptionValue("out"));
+        File dir  = file.getParentFile();
+        if ( !dir.exists() ) { dir.mkdirs(); }
+        return file;
+    }
+
+    protected MongoEDMHarvester getHarvester(CommandLine line)
+    {
+        String host   = line.getOptionValue("host");
+        String dbName = line.getOptionValue("db", "europeana");
+        int    port   = Integer.parseInt(line.getOptionValue("port", "27017"));
+
+        MongoClient   client = new MongoClient(host, port);
+        MongoDatabase db     = client.getDatabase(dbName);
+        return new MongoEDMHarvester(db, null);
+    }
+
+    protected HarvesterCallback getCallback(CommandLine line
+                                          , EDMTurtleWriter writer)
+    {
+        return new HarvesterCallback() {
+            @Override
+            public void handle(Resource r)
+            {
+                Model m = r.getModel();
+                try                  { writer.write(m);               }
+                catch(IOException e) { throw new RuntimeException(e); }
+                finally              { m.removeAll();                 }
+            }
+        };
+    }
+
+    protected void handleCmd(MongoEDMHarvester harvester, HarvesterCallback cb
+                           , CommandLine line) throws Throwable
+    {
+        if ( line.hasOption("all") ) { harvester.harvestAll(cb); return; }
+
+        if ( line.hasOption("uris") ) {
+            List<String> uris = Arrays.asList(line.getOptionValues("uris"));
+            harvester.harvest(uris, cb);
+            return;
+        }
+
+        if ( line.hasOption("file") ) {
+            File         file = new File(line.getOptionValue("file"));
+            List<String> uris = FileUtils.readLines(file);
+            harvester.harvest(uris, cb);
+            return;
+        }
+
+        if ( line.hasOption("search") ) {
+            harvester.harvestBySearch(line.getOptionValue("search"), cb);
+            return;
+        }
+    }
+}
---
+++ ld-toolkit/src/main/java/eu/europeana/ld/toolkit/ToolkitCmd.java
@@ -0,0 +1,99 @@
+/**
+ * 
+ */
+package eu.europeana.ld.toolkit;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.util.Properties;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.GnuParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.OptionBuilder;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+
+/**
+ * @author Hugo Manguinhas <hugo.manguinhas@europeana.eu>
+ * @since 17 Oct 2016
+ */
+public abstract class ToolkitCmd
+{
+    protected static final Properties PROPERTIES = new Properties();
+    
+    protected static void loadProperties(String cfg)
+    {
+        try {
+            InputStream is = ToolkitCmd.class.getResourceAsStream(cfg);
+            PROPERTIES.loadFromXML(is);
+        }
+        catch (IOException e) { throw new RuntimeException(e); }
+    }
+
+    protected static String getProperty(String key)
+    {
+        return PROPERTIES.getProperty(key);
+    }
+
+    private HelpFormatter _formatter = new HelpFormatter();
+    private PrintStream   _ps        = System.out;
+
+    public ToolkitCmd() {}
+
+    public void process(String[] args)
+    {
+        //Build Options
+        Options opts = buildOptions();
+
+        //Create parser
+        CommandLineParser parser = new GnuParser();
+        try {
+            // parse the command line arguments
+            CommandLine line = parser.parse(opts, args);
+            if(line.hasOption("help")) { printUsage(opts); return; }
+
+            printHeader();
+            try                { process(line); }
+            catch(Throwable t) { printError(t); }
+        }
+        catch(ParseException exp)
+        {
+            //oops, something went wrong
+            printUsage(opts, exp.getMessage());
+        }
+    }
+
+    protected abstract Options buildOptions();
+
+    protected abstract void process(CommandLine line) throws Throwable;
+
+    protected void printUsage(Options opts)
+    {
+        String name = getProperty("info.name");
+        _formatter.printHelp(name, opts, true);
+    }
+
+    protected void printUsage(Options opts, String msg)
+    {
+        printHeader();
+        _ps.println();
+        _ps.println(msg);
+        _ps.println();
+        printUsage(opts);
+    }
+
+    protected void printHeader()
+    {
+        _ps.print(PROPERTIES.getProperty("layout.header"));
+    }
+
+    protected void printError(Throwable t)
+    {
+        _ps.println();
+        _ps.println("Error: " + t.getMessage());
+    }
+}
